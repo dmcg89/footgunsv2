b72b583bb0865f893aaa71a914503d5d3299f732
Michael McGowan <crypto.dmc.trade@gmail.com>
2022-03-21 22:08:47 -0700

    init

---
+++ src/context/TransactionContext.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+import { contractABI, contractAddress } from "../utils/constants";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionsContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+
+  console.log(provider, signer, transactionsContract);
+
+  return transactionsContract;
+};
+
+export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [formData, setFormData] = useState({
+    addressTo: "",
+    amount: "",
+    keyword: "",
+    message: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [transactionCount, settransactionCount] = useState(
+    localStorage.getItem("transactionCount")
+  );
+  const [transactions, setTransactions] = useState([]);
+
+  // set up small react app to play with change with form data 1:53ish
+  const handleChange = (e, name) => {
+    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
+  };
+  const checkIfWalletIsConnected = async () => {
+    if (!ethereum) return alert("Please install metamask");
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setCurrentAccount(accounts[0]);
+      console.log("current account");
+      console.log(accounts[0]);
+    } catch (error) {
+      console.log(error);
+      throw new Error("No ethereum object");
+    }
+  };
+
+  const sendTransaction = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+      // get the data from the form
+      if (ethereum) {
+        const { addressTo, amount, keyword, message } = formData;
+        const transactionContract = getEthereumContract();
+        const parsedAmount = ethers.utils.parseEther(amount);
+        console.log(`Current Account - ${currentAccount}`);
+        await ethereum.request({
+          method: "eth_sendTransaction",
+          params: [
+            {
+              from: currentAccount,
+              to: addressTo,
+              gas: "0x5208", //21000 gwei
+              value: parsedAmount._hex,
+            },
+          ],
+        });
+        console.log("here2");
+        const transactionHash = await transactionContract.addToBlockchain(
+          addressTo,
+          parsedAmount,
+          message,
+          keyword
+        );
+        // console.log("here2");
+        setIsLoading(true);
+        console.log(`Loading - ${transactionHash.hash}`);
+        await transactionHash.wait();
+        setIsLoading(false);
+        console.log(`Success - ${transactionHash.hash}`);
+
+        const transactionCount =
+          await transactionContract.getTransactionCount();
+
+        settransactionCount(transactionCount.toNumber());
+      }
+    } catch (error) {
+      console.log("error here");
+      console.log(error);
+      throw new Error("No ethereum object");
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+  return (
+    <TransactionContext.Provider
+      value={{
+        connectWallet,
+        transactions,
+        currentAccount,
+        formData,
+        setFormData,
+        handleChange,
+        sendTransaction,
+      }}
+    >
+      {children}
+    </TransactionContext.Provider>
+  );
+};
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x7568E083169447B2E30097aB1D6B440f6EfAbF32";
